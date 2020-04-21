c77ec0d07731f34b935b05ac31d8aa6da4370cbb
jjruanlili <525162140@qq.com>
2020-04-21 15:21:53 +0800

    Add files via upload

---
+++ Son.java
@@ -0,0 +1,22 @@
+package bishiti;
+//子类与父类初始化过程 子类
+public class Son extends Father {
+    static {
+        System.out.println("son{}");
+    }
+
+    public Son(){
+        System.out.println("son constructor");
+    }
+
+
+    public static void main(String[] args) {
+        Son s1 = new Son();
+        //输出:
+//        son{}
+//        father {}
+//        father constructor
+//        son constructor
+        //如果把 father {}的static去掉会有不一样的结果,好奇的请自己尝试
+    }
+}
